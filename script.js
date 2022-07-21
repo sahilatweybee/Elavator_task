2d02eddc947baaab1641c0bea952f8bafe745e4f
Sahil Parsaniya <sahil@webtheuniverse.com>
2022-07-21 18:07:13 +0530

    completed static animetion

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 //<------------------------------------------------Global Variables-------------------->//
 let maxHeight = 700;
 let positionBtm = 0;
-let animation = null;
 
-let array = [
-    { id: 1, checked: false, floor: 1 },
-    { id: 2, checked: false, floor: 1 },
-    { id: 3, checked: false, floor: 1 }
+
+let Elevators = [
+    { id: 1, checked: false, floor: 1, movingEL: false },
+    { id: 2, checked: false, floor: 1, movingEL: false },
+    { id: 3, checked: false, floor: 1, movingEL: false }
 ];
 
 let floor = [
@@ -20,12 +20,13 @@ let floor = [
 ];
 
 
+
 //<-----------------------------------------------------Methods------------------------>//
 const check = function (i) {
-    const index = array.findIndex(x => x.id == i);
-    array[index].checked = !array[index].checked;
+    const index = Elevators.findIndex(x => x.id == i);
+    Elevators[index].checked = !Elevators[index].checked;
 
-    for (let el of array) {
+    for (let el of Elevators) {
         if (el.checked == true) {
             document.querySelector(`.elevator-${el.id}`).style.bottom = '0px';
             document.querySelector(`.elevator-${el.id}`).style.opacity = '0.4';
@@ -35,7 +36,7 @@ const check = function (i) {
         } else {
             document.querySelector(`.elevator-${el.id}`).style.border = 'none';
             document.querySelector(`.elevator-${el.id}`).style.opacity = '0.8';
-            if (el.floor > array.length) {
+            if (el.floor > Elevators.length) {
                 el.floor = 1;
             }
             else {
@@ -46,46 +47,67 @@ const check = function (i) {
 };
 
 const ElevatorMovments = function (i) {
-    var closeEL = array.map(el => el.floor).reduce((prev, curr) => {
+    let animate = null;
+    let closeEL = Elevators.map(el => el.floor).reduce((prev, curr) => {
         return (Math.abs(curr - i) < Math.abs(prev - i) ? curr : prev);
     });
-    for (let el of array) {
-        if (closeEL == el.floor && el.checked == false) {
-
-            positionBtm = Number((el.floor - 1) * 140);
+    for (let lift of Elevators) {
+        if (closeEL == lift.floor && !lift.checked) {
+            positionBtm = Number((lift.floor - 1) * 140);
             let distBtm = 140 * (i - 1);
-            let pb = positionBtm;
-
-            clearInterval(animation);
-            animation = setInterval(move(), 3000);
-
-            function move() {
-                if (positionBtm == distBtm) {
-                    clearInterval(animation);
+            let tempFloorBtm = positionBtm;
+            let tempFloor = lift.floor;
+            clearInterval(animate);
+            animate = setInterval(function () {
+                if (positionBtm == distBtm && lift.movingEL == true) {
+                    lift.movingEL = false;
+                    // animateDoors = setInterval(function () {
+                    //     if (doorPosition == 0) {
+                    //         clearInterval(animateDoors);
+                    //     } else {
+                    //         doorPosition--;
+                    //         doorLeft.style.left -= `${doorPosition}px`;
+                    //         doorRight.style.right -= `${doorPosition}px`;
+                    //     }
+                    // }, 5);
+                    clearInterval(animate);
                 } else {
-                    if (positionBtm < distBtm) {
-                        positionBtm++;
-                        document.querySelector(`.elevator-${el.id}`).style.bottom = `${positionBtm}px`;
-                        if (pb == positionBtm) {
-                            pb += 140;
-                            document.querySelector(`.indicator-${el.id}`).innerHTML = el.floor;
-                            el.floor++;
-                        }
-                    } else if (positionBtm > distBtm) {
 
-                        positionBtm--;
-                        document.querySelector(`.elevator-${el.id}`).style.bottom = `${positionBtm}px`;
-                        if (pb == positionBtm) {
-                            pb -= 140;
-                            document.querySelector(`.indicator-${el.id}`).innerHTML = el.floor;
-                            el.floor--;
+                    if (positionBtm < distBtm) {
+                        lift.movingEL = true;
+                        if (tempFloorBtm === positionBtm) {
+                            lift.floor = i;
+                            tempFloorBtm += 140;
+                            tempFloor++;
+                            document.querySelector(`.indicator-${lift.id}`).innerHTML = tempFloor;
                         }
+                        positionBtm++;
+                        document.querySelector(`.elevator-${lift.id}`).style.bottom = `${positionBtm}px`;
+
+                    } else if (positionBtm > distBtm) {
+                        lift.movingEL = true;
+                        if (tempFloorBtm === positionBtm) {
+                            lift.floor = i;
+                            tempFloorBtm -= 140;
+                            tempFloor--;
+                            document.querySelector(`.indicator-${lift.id}`).innerHTML = tempFloor;
+                        }
+                        positionBtm--;
+                        document.querySelector(`.elevator-${lift.id}`).style.bottom = `${positionBtm}px`;
                     }
+                    lift.movingEL = false;
                 }
-            }
+            }, (Math.abs(i - lift.floor)));
+            lift.floor = i;
             break;
         }
     }
 };
 
+const btnUp = function (i) {
+    ElevatorMovments(i);
+}
 
+const btnDown = function (i) {
+    ElevatorMovments(i);
+}
